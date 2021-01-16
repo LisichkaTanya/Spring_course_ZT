32abb4a8f08dcba7fd9293e05ca4f23255700748
Tatiana Solomatina <lisichka-tanya@mail.ru>
2021-01-16 12:37:53 +0300

    @Component @Autowired

--- src/spring_introduction/Cat.java
+++ src/spring_introduction/Cat.java
@@ -1,5 +1,10 @@
 package spring_introduction;
 
+import org.springframework.stereotype.Component;
+
+@Component("catBean") //Если не указать в () id класса, то он создастся по-умолчанию из названия класса
+                    // только первая буква будет прописной "cat"
+                    //MyDog -> myDog; SQLClass -> SQLClass
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,19 +1,23 @@
 package spring_introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("personBean")
 public class Person {
     private Pet pet;
     private String surname;
     private int age;
 
-    //Этот конструктор мы использовали для внедрения DI методом конструктор
-//    public Person(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
-
-    public Person() {
+    @Autowired
+    public Person(Pet pet) {
         System.out.println("Person bean is created");
+        this.pet = pet;
     }
+
+//    public Person() {
+//        System.out.println("Person bean is created");
+//    }
 
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
