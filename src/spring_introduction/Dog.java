b024b7ce4e4df7125462afe652a58eff4e0818dd
Tatiana Solomatina <lisichka-tanya@mail.ru>
2021-01-16 11:15:23 +0300

    Bean scope

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,6 +1,8 @@
 package spring_introduction;
 
 public class Dog implements Pet{
+    private String name;
+
     public Dog() {
         System.out.println("Dog bean is created");
     }
@@ -8,5 +10,13 @@ public class Dog implements Pet{
     @Override
     public void say() {
         System.out.println("Bow-wow");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
