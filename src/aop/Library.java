35bd6cc91921bf36ad50d8722c4588c0184cd986
Tatiana Solomatina <lisichka-tanya@mail.ru>
2021-01-17 17:59:18 +0300

    AOP Advice тип @Before

---
+++ src/aop/Library.java
@@ -0,0 +1,10 @@
+package aop;
+
+import org.springframework.stereotype.Component;
+
+@Component("libraryBean")
+public class Library {
+    public void getBook() {
+        System.out.println("Мы берём книгу");
+    }
+}
